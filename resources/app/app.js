d2fada1a5b14135ec31a7fbbb83ec50138e65159
Rafael Castro <rccastrorafael@gmail.com>
2020-12-16 09:36:02 -0300

    fix empty coverage file

--- resources/app/app.js
+++ resources/app/app.js
@@ -1,1 +1,3 @@
-console.log('teste')
+export default function sum(a, b) {
+  return a + b
+}
--- resources/app/app.test.js
+++ resources/app/app.test.js
@@ -1,6 +1,4 @@
-function sum(a, b) {
-  return a + b
-}
+import sum from './app'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
