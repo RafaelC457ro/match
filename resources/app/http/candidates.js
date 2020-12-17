021e3637e0d22dedb249f36be924d14c521c14bc
Rafael Castro <rccastrorafael@gmail.com>
2020-12-16 21:08:01 -0300

    basic implementation

---
+++ resources/app/http/candidates.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const getCandidates = async (filter) => {
+  console.log(filter)
+  const { cities, experience, technologies } = filter
+  const city = cities ? `city[]=${cities.join(',')}` : ''
+  const techs = technologies ? `&technologies[]=${cities.join(',')}` : ''
+
+  let exp = ''
+  if (experience) {
+    exp = experience.endExperience
+      ? `&betweenStart=${experience.startExperience}&betweenEnd=${experience.endExperience}`
+      : `&greaterThen=${experience.startExperience}`
+  }
+
+  const response = await axios.get(`/api/v1/candidates?${city}${techs}${exp}`)
+  return response.data
+}
